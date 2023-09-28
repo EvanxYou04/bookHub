b7c44653678f2f4d271e3f99bc00c80f20484e29
Evan <evanyou2004@gmail.com>
2023-09-27 23:13:14 -0500

    created modal that toggles on click of add book button.

--- main.js
+++ main.js
@@ -6,6 +6,25 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+const addButton = document.querySelector("#addBook");
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".close-button");
+
+addButton.addEventListener("click", () => {
+    toogleModal();
+});
+closeButton.addEventListener("click", () => {
+    toogleModal();
+});
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        toogleModal();
+    }
+});
+
+function toogleModal() {
+    modal.classList.toggle("show-modal");
+}
 
 function addBookToLibrary() {
     // add object Book to myLibrary
