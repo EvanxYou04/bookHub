e22073d89979d651b37f2c694bcf925c8bbdc326
Evan <evanyou2004@gmail.com>
2023-10-04 12:00:06 -0500

    added read button toggle to book card.

--- main.js
+++ main.js
@@ -30,6 +30,25 @@ function toogleModal() {
     modal.classList.toggle("show-modal");
 }
 
+const testBook = new Book("test", "test", 123, true);
+myLibrary.push(testBook);
+displayBook(testBook);
+
+//change color of button to green and red when clicked
+const bookCards = document.querySelectorAll(".book-card");
+bookCards.forEach((bookCard) => {
+    const readButton = bookCard.querySelector("button");
+    readButton.addEventListener("click", () => {
+        readButton.classList.toggle("read-button");
+        readButton.textContent =
+            readButton.textContent === "Have read"
+                ? "Have not read"
+                : "Have read";
+        readButton.style.backgroundColor =
+            readButton.textContent === "Have read" ? "green" : "red";
+    });
+});
+
 function addBookToLibrary() {
     // add object Book to myLibrary
     const title = document.querySelector("#title").value;
@@ -38,7 +57,7 @@ function addBookToLibrary() {
     const read = document.querySelector("#haveRead").checked;
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
-    displayBooks();
+    displayBook(book);
     toogleModal();
     // clear input fields
     document.querySelector("#title").value = "";
@@ -46,26 +65,23 @@ function addBookToLibrary() {
     document.querySelector("#pages").value = "";
     document.querySelector("#haveRead").checked = false;
 }
-function displayBooks() {
-    // display all books in myLibrary
-    myLibrary.forEach((book) => {
-        const bookCard = document.createElement("div");
-        const title = document.createElement("p");
-        const author = document.createElement("p");
-        const pages = document.createElement("p");
-        const read = document.createElement("p");
+function displayBook(book) {
+    const bookCard = document.createElement("div");
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const read = document.createElement("button");
 
-        bookCard.classList.add("book-card");
+    bookCard.classList.add("book-card");
 
-        title.textContent = book.title;
-        author.textContent = book.author;
-        pages.textContent = book.pages;
-        read.textContent = book.read;
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    read.textContent = book.read ? "Have read" : "Have not read";
 
-        bookCard.appendChild(title);
-        bookCard.appendChild(author);
-        bookCard.appendChild(pages);
-        bookCard.appendChild(read);
-        document.querySelector(".book-grid").appendChild(bookCard);
-    });
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(read);
+    document.querySelector(".book-grid").appendChild(bookCard);
 }
